a740b9437436c234be9e931f378208e653cfe84a
mdmudassir0143 <mdmudassir0143@gmail.com>
2024-10-22 12:27:01 +0530

    initials

---
+++ api/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/api/random-number', (req, res) => {
+    const min = 44552518;
+    const max = 44554718;
+    const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+    res.json({ randomNumber });
+});
+
+module.exports = app;
