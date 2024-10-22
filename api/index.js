282ad6168d7a1cffc24bf0d0b06930d7915f0538
mdmudassir0143 <mdmudassir0143@gmail.com>
2024-10-22 12:35:46 +0530

    range extended

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get('/api/random-number', (req, res) => {
     const min = 44552518;
-    const max = 44554718;
+    const max = 44553518;
     const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
     res.json({ randomNumber });
 });
